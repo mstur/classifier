75b7087bc05bfaf1f9302e46e972014017d84edf
Mia <miastu101@gmail.com>
2018-01-08 19:27:32 -0600

    initial commit

---
+++ get_data.py
@@ -0,0 +1,221 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import gzip
+import numpy
+
+from tensorflow.contrib.learn.python.learn.datasets import base
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import random_seed
+from tensorflow.python.platform import gfile
+
+DEFAULT_SOURCE_URL = '/datasets/'  # possibly change to internet-hosted source
+NUM_CLASSES = 12
+PIXELS = 784
+
+TRAIN_IMAGES = 'train-images.gz'
+TRAIN_LABELS = 'train-labels.gz'
+TEST_IMAGES = 'test-images.gz'
+TEST_LABELS = 'test-labels.gz'
+
+
+def _read32(bytestream):
+    dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
+
+
+def extract_images(f):
+    """Extract the images into a 4D uint8 numpy array [index, y, x, depth].
+    Args:
+      f: A file object that can be passed into a gzip reader.
+    Returns:
+      get_data: A 4D uint8 numpy array [index, y, x, depth].
+    Raises:
+      ValueError: If the bytestream does not start with 2051.
+    """
+    print('Extracting', f.name)
+    with gzip.GzipFile(fileobj=f) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2051:
+            raise ValueError('Invalid magic number %d in MNIST image file: %s' % (magic, f.name))
+        num_images = _read32(bytestream)
+        rows = _read32(bytestream)
+        cols = _read32(bytestream)
+        buf = bytestream.read(rows * cols * num_images)
+        data = numpy.frombuffer(buf, dtype=numpy.uint8)
+        data = data.reshape(num_images, rows, cols, 1)
+    return data
+
+
+def dense_to_one_hot(labels_dense):
+    """Convert class labels from scalars to one-hot vectors."""
+    num_labels = labels_dense.shape[0]
+    index_offset = numpy.arange(num_labels) * NUM_CLASSES
+    labels_one_hot = numpy.zeros((num_labels, NUM_CLASSES))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+
+def extract_labels(f):
+    """Extract the labels into a 1D uint8 numpy array [index].
+    Args:
+      f: A file object that can be passed into a gzip reader.
+    Returns:
+      labels: a 1D uint8 numpy array.
+    Raises:
+      ValueError: If the bystream doesn't start with 2049.
+    """
+    print('Extracting', f.name)
+    with gzip.GzipFile(fileobj=f) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2049:
+            raise ValueError('Invalid magic number %d in label file: %s' % (magic, f.name))
+        num_items = _read32(bytestream)
+        buf = bytestream.read(num_items)
+        labels = numpy.frombuffer(buf, dtype=numpy.uint8)
+        return dense_to_one_hot(labels)
+
+
+class DataSet(object):
+
+    def __init__(self,
+                 images,
+                 labels,
+                 dtype=dtypes.float32,
+                 reshape=True,
+                 seed=None):
+        """Construct a DataSet.
+        one_hot arg is used only if fake_data is true.  `dtype` can be either
+        `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+        `[0, 1]`.  Seed arg provides for convenient deterministic testing.
+        """
+        seed1, seed2 = random_seed.get_seed(seed)
+        # If op level seed is not set, use whatever graph level seed is returned
+        numpy.random.seed(seed1 if seed is None else seed2)
+        dtype = dtypes.as_dtype(dtype).base_dtype
+        if dtype not in (dtypes.uint8, dtypes.float32):
+            raise TypeError('Invalid image dtype %r, expected uint8 or float32' % dtype)
+        assert images.shape[0] == labels.shape[0], (
+            'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+        self._num_examples = images.shape[0]
+
+        # Convert shape from [num examples, rows, columns, depth]
+        # to [num examples, rows*columns] (assuming depth == 1) -- flattens each image
+        if reshape:
+            assert images.shape[3] == 1
+            images = images.reshape(images.shape[0], images.shape[1] * images.shape[2])
+        if dtype == dtypes.float32:
+            # Scale from [0, 255] -> [0.0, 1.0].
+            images = images.astype(numpy.float32)
+            images = numpy.multiply(images, 1.0 / 255.0)
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size, shuffle=True):
+        """Return the next `batch_size` examples from this data set."""
+        start = self._index_in_epoch
+        # Shuffle for the first epoch
+        if self._epochs_completed == 0 and start == 0 and shuffle:
+            perm0 = numpy.arange(self._num_examples)
+            numpy.random.shuffle(perm0)
+            self._images = self.images[perm0]
+            self._labels = self.labels[perm0]
+        # Go to the next epoch
+        if start + batch_size > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Get the rest examples in this epoch
+            rest_num_examples = self._num_examples - start
+            images_rest_part = self._images[start:self._num_examples]
+            labels_rest_part = self._labels[start:self._num_examples]
+            # Shuffle the data
+            if shuffle:
+                perm = numpy.arange(self._num_examples)
+                numpy.random.shuffle(perm)
+                self._images = self.images[perm]
+                self._labels = self.labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size - rest_num_examples
+            end = self._index_in_epoch
+            images_new_part = self._images[start:end]
+            labels_new_part = self._labels[start:end]
+            return numpy.concatenate((images_rest_part, images_new_part), axis=0) , numpy.concatenate((labels_rest_part, labels_new_part), axis=0)
+        else:
+            self._index_in_epoch += batch_size
+            end = self._index_in_epoch
+            return self._images[start:end], self._labels[start:end]
+
+
+def read_data_sets(train_dir,
+                   pixels,
+                   classes,
+                   dtype=dtypes.float32,
+                   reshape=True,
+                   validation_size=50,
+                   seed=None,
+                   source_url=DEFAULT_SOURCE_URL,):
+    global PIXELS
+    PIXELS= pixels
+    global NUM_CLASSES
+    NUM_CLASSES= classes
+
+    if not source_url:  # empty string check
+        source_url = DEFAULT_SOURCE_URL
+
+    local_file = base.maybe_download(TRAIN_IMAGES, train_dir,
+                                     source_url + TRAIN_IMAGES)
+    with gfile.Open(local_file, 'rb') as f:
+        train_images = extract_images(f)
+
+    local_file = base.maybe_download(TRAIN_LABELS, train_dir,
+                                     source_url + TRAIN_LABELS)
+    with gfile.Open(local_file, 'rb') as f:
+        train_labels = extract_labels(f)
+
+    local_file = base.maybe_download(TEST_IMAGES, train_dir,
+                                     source_url + TEST_IMAGES)
+    with gfile.Open(local_file, 'rb') as f:
+        test_images = extract_images(f)
+
+    local_file = base.maybe_download(TEST_LABELS, train_dir,
+                                     source_url + TEST_LABELS)
+    with gfile.Open(local_file, 'rb') as f:
+        test_labels = extract_labels(f)
+
+    if not 0 <= validation_size <= len(train_images):
+        raise ValueError(
+            'Validation size should be between 0 and {}. Received: {}.'
+            .format(len(train_images), validation_size))
+
+    validation_images = train_images[:validation_size]
+    validation_labels = train_labels[:validation_size]
+    train_images = train_images[validation_size:]
+    train_labels = train_labels[validation_size:]
+
+    options = dict(dtype=dtype, reshape=reshape, seed=seed)
+
+    train = DataSet(train_images, train_labels, **options)
+    validation = DataSet(validation_images, validation_labels, **options)
+    test = DataSet(test_images, test_labels, **options)
+
+    return base.Datasets(train=train, validation=validation, test=test)
